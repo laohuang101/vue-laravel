9939addee9a06111bdb2ccc5bb8c69c3425fb55e
WONG KAM LOKE <wkamloke@gmail.com>
2025-05-21 20:06:36 +0800

    res file

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>HELLO! {{ counter }}</h1>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const counter = ref(0);
+setInterval(() => {
+    counter.value++;
+}, 1000);
+</script>
